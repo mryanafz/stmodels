d5dc73ce6f1b16d6bd7eb433f34d9d99a901548e
RyanAfrizal <ryan.afrizal55@gmail.com>
2021-12-28 02:13:48 +0800

    'fix'

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import numpy as np
 import pandas as pd
 import pickle
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
