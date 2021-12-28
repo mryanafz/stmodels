cceebb5be1ad586c9143e7770366b7ab75b7beed
RyanAfrizal <ryan.afrizal55@gmail.com>
2021-12-28 13:28:22 +0800

    Alhamdulillah

--- app.py
+++ app.py
@@ -36,15 +36,15 @@ def index():
             'Jenis Kelamin': int(request.form.get('jenisKelamin')),
         }]
 
-        print(test)
+        # print(test)
 
-        # predict = model.predict(pd.DataFrame(test))
+        predict = model.predict(pd.DataFrame(test))
         # response = jsonify(result={
         #     'perdict': str(predict[0]),
         # })
         # response.headers.add('Access-Control-Allow-Origin', '*')
         # return response
-        return jsonify(test)
+        return jsonify(predict[0])
     elif request.method == "GET":
         return f"flask : {flask.__version__}\n numpy : {np.__version__}\n pandas : {pd.__version__}\n sklearn : {sklearn.__version__}\n xgboost : {xgboost.__version__}\n jcopml : {jcopml.__version__}\n python : {python_version()}\n"
 
